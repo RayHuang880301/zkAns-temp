3f37851926bd249358ea069928bb9e9cc24ab9fa
abner <fantasrulai@gmail.com>
2023-01-24 00:21:08 +0800

    update

--- scripts/server.ts
+++ scripts/server.ts
@@ -6,39 +6,39 @@ const express = require('express');
 const { genWasm } = require('../utils/genWasm');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use('/', express.static(path.resolve(__dirname, '../public')));
 
-var server = http.createServer(async (req: any, res: any) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    const _url = req.url == "/" ? "/index.html" : req.url;
-    const urlObj = url.parse(_url, true);
-    if (urlObj.pathname == "./circuit.wasm") {
-        await genWasm(urlObj.query.ans, () => {
-            console.log(urlObj.pathname, urlObj.query.ans);
-            fs.readFile('/ans-circuits/circuit_js' + urlObj.pathname, (err: any, data: any) => {
-                if (err) {
-                    res.writeHead(404);
-                    res.write('File Not Found');
-                }
-                else 
-                    res.write(data);
-                res.end();
-            });
-        });
-    }
-    else
-        fs.readFile('./public' + urlObj.pathname, (err: any, data: any) => {
-            if (err) {
-                res.writeHead(404);
-                res.write('File Not Found');
-            }
-            else {
-                res.write(data);
-            }
-            res.end();
-        });
-});
+// var server = http.createServer(async (req: any, res: any) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     const _url = req.url == "/" ? "/index.html" : req.url;
+//     const urlObj = url.parse(_url, true);
+//     if (urlObj.pathname == "./circuit.wasm") {
+//         await genWasm(urlObj.query.ans, () => {
+//             console.log(urlObj.pathname, urlObj.query.ans);
+//             fs.readFile('/ans-circuits/circuit_js' + urlObj.pathname, (err: any, data: any) => {
+//                 if (err) {
+//                     res.writeHead(404);
+//                     res.write('File Not Found');
+//                 }
+//                 else 
+//                     res.write(data);
+//                 res.end();
+//             });
+//         });
+//     }
+//     else
+//         fs.readFile('./public' + urlObj.pathname, (err: any, data: any) => {
+//             if (err) {
+//                 res.writeHead(404);
+//                 res.write('File Not Found');
+//             }
+//             else {
+//                 res.write(data);
+//             }
+//             res.end();
+//         });
+// });
 
-server.listen(5500);
+app.listen(5500);
 
 console.log('Server at port 5500 is running')
